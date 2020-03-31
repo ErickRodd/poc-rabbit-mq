5d56283283c140561db41604a1af0011e0d40ee6
erickrodd <erick.rod1100gmail.com>
2020-03-31 11:28:58 -0300

    Removing public from repository

--- src/main/java/com/poc/rabbitMQ/input/InputRepository.java
+++ src/main/java/com/poc/rabbitMQ/input/InputRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InputRepository extends JpaRepository<Input, Long> {
+interface InputRepository extends JpaRepository<Input, Long> {
 }
