12f9756432570fa80e3cb93455da440851a99aed
erickrodd <erick.rod1100gmail.com>
2020-04-06 10:48:41 -0300

    Added cause message in exception thrower.

--- src/main/java/com/poc/rabbitMQ/utils/ThrowExceptionUtil.java
+++ src/main/java/com/poc/rabbitMQ/utils/ThrowExceptionUtil.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class ThrowExceptionUtil {
     public static void randomIOException() throws IOException {
         if (new Random().nextBoolean()) {
-            throw new IOException("Erro gerado aleatóriamente para testar o delay do reenvio de mensagens rejeitadas.");
+            throw new IOException("Erro gerado aleatóriamente para testar o delay do reenvio de mensagens rejeitadas.", new IOException("Erro gerado aleatóriamente para testar o delay do reenvio de mensagens rejeitadas."));
         }
     }
 }
